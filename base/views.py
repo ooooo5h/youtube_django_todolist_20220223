76c3501d7bba8428c73a081bab974906e5dc3209
전은형 <ooooo5h91@gmail.com>
2022-02-24 20:58:42 +0900

    view 생성

--- base/views.py
+++ base/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from .models import Task
 
 from django.contrib.auth.views import LoginView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
@@ -16,6 +18,12 @@ class CustomLoginView(LoginView):
     
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+class RegisterPage(FormView):
+    template_name = 'base/register'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
 
 
 class TaskList(LoginRequiredMixin, ListView):
