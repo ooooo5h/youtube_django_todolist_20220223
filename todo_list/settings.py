239364dffdfad38fff7ce1e832b46e64ddc7a38f
전은형 <ooooo5h91@gmail.com>
2022-02-25 23:36:23 +0900

    프로젝트 생성 후 해야할 일 1
    장고와 앱을 연결해주고, time_zone을 한국으로 설정하기

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig'   # 장고에게 base라는 앱이 있다고 알려줘야함
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
